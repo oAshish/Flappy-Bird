ef6ed730ee66cb830f58c97bc392ec2a0a3df934
ASHISH PARAJULI <121391998+oAshish@users.noreply.github.com>
2023-11-24 15:22:48 +0545

    Update Pipe.h

--- Pipe.h
+++ Pipe.h
@@ -1,13 +1,18 @@
 #pragma once
 #include<SFML/Graphics.hpp>
-
 class Pipe
 {
-
 public:
-	Pipe(int);
-	sf::Texture pipeup, pipedown;
-	static void loadtexture();
-	
+	Pipe(float);
+	sf::Sprite sprite_up, sprite_down;
+	static void loadTextures();
+	void update(sf::Time&);
+	float getRightBound();
+
+private:
+	static sf::Texture pipe_up, pipe_down;
+	static int pipe_distance, move_speed;
+
+
 };
 
