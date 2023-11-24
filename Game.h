cc1398aac294049bc4a6eb15dad0bbbd5f7d2c37
ASHISH PARAJULI <121391998+oAshish@users.noreply.github.com>
2023-11-24 15:24:48 +0545

    Update Game.h

--- Game.h
+++ Game.h
@@ -1,19 +1,36 @@
 #pragma once
 #include<SFML/Graphics.hpp>
 #include"Bird.h"
+#include<vector>
+#include"Pipe.h"
+#include<random>
 class Game
 {
 public:
-	Game(sf::RenderWindow& window);//creating a contructor which takes parameter .......
-	sf::RenderWindow& win;         //Its like declaring a win variable of type sf::RenderWindow&
-	void startgameloop();   //member function of game.
+	Game(sf::RenderWindow& window);
+	sf::RenderWindow& win;
+	void startgameloop();
 private:
 	sf::Texture bg_texture, ground_texture;
-	sf:: Sprite bg_sprite,ground_sprite1,ground_sprite2;
-	void draw();
+	sf::Sprite bg_sprite, ground_sprite1, ground_sprite2;
 	Bird bird;
-	bool is_enter,run_game;
-    const int move_speed = 250;
-	void moveground(sf::Time &dt);
+	bool is_enter_pressed, run_game,start_monitoring;
+	const int move_speed = 270;
+	void draw();
+	void moveGround(sf::Time&);
+	void doProcessing(sf::Time& dt);
+	void checkcollisions();
+	void restart_game();
+	void check_scores();
+	int pipe_counter, pipe_spawn_time,score;
+	std::string int_to_string(int);
+	std::vector<Pipe> pipes;
+	std::random_device rd;
+	std::uniform_int_distribution<int> dist{250, 550};
+	sf::Font font;
+	sf::Text restart_text,score_text;
+
+
+
 };
 
